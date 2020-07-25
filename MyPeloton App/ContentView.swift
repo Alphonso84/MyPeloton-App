c3674f0ffb6180e196969b9c402764182fe09429
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2020-07-25 17:54:20 -0400

    Initial Commit

---
+++ MyPeloton App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MyPeloton App
+//
+//  Created by Alphonso Sensley II on 7/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyPeloton App/MyPeloton_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPeloton_AppApp.swift
+//  MyPeloton App
+//
+//  Created by Alphonso Sensley II on 7/25/20.
+//
+
+import SwiftUI
+
+@main
+struct MyPeloton_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
