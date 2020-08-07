1bdb0862d4a99b8d3c1bb4ab5eaec2827b496cf5
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2020-08-06 20:20:29 -0400

    refactor

--- MyPeloton App/ContentView.swift
+++ MyPeloton App/ContentView.swift
@@ -14,62 +14,30 @@ struct ContentView: View {
         
         NavigationView {
             VStack {
-            Picker(selection: $pickerSelectedItem, label: Text(""), content: /*@START_MENU_TOKEN@*/{
-                Text("20 min").tag(0)
-                Text("30 min").tag(1)
-                Text("45 min").tag(2)
-                Text("60 min").tag(3)
-                Text("90 min").tag(4)
-            }/*@END_MENU_TOKEN@*/)
-            .padding(.top, 10)
-            .pickerStyle(SegmentedPickerStyle())
-            List(workouts, id: \.workoutTimestamp) { workout in
-                ZStack(alignment: .leading) {
-                    HStack {
-                        Image("\(workoutImage(instructorName: workout.instructorName))")
-                            .resizable()
-                            .frame(width: 59, height: 70)
-                            .cornerRadius(40)
-                        VStack(alignment:.leading) {
-                            Text("\(workout.instructorName)")
-                                .font(.title)
-                            Text("\(workout.fitnessDiscipline.rawValue)")
-                                .foregroundColor(.gray)
-                            Text("\(workout.title)")
-                                .foregroundColor(.gray)
-                                .font(.subheadline)
-                            Text("\(workout.workoutTimestamp)")
-                                .foregroundColor(.blue)
-                                .font(.subheadline)
-                        }
-                    }
-                    
-                    NavigationLink(
-                        destination: WorkoutChartView(),
-                        label: {})
-                }
-            }
-            .navigationBarTitle("Peloton Workouts", displayMode: .inline)
-            .navigationBarItems(leading: Image("Peloton")
-                                    .resizable()
-                                    .cornerRadius(15)
-                                    .frame(width: 35, height: 35, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/))
-        }.onAppear(perform: {
-            decodeJSON()
-        })
-    }
-    }
-    
-    
-    func workoutImage(instructorName:String) ->String {
-        var imageString = String()
-        if instructorName.isEmpty {
-            imageString = "Peloton"
-        } else {
-            imageString = instructorName
+                Picker(selection: $pickerSelectedItem, label: Text(""), content: /*@START_MENU_TOKEN@*/{
+                    Text("20 min").tag(0)
+                    Text("30 min").tag(1)
+                    Text("45 min").tag(2)
+                    Text("60 min").tag(3)
+                    Text("90 min").tag(4)
+                }/*@END_MENU_TOKEN@*/)
+                .padding(.top, 10)
+                .pickerStyle(SegmentedPickerStyle())
+                
+                WorkoutRowView(workouts: workouts)
+                    .navigationBarTitle("All Workouts", displayMode:.inline)
+                    .navigationBarItems(leading: Image("Peloton")
+                                            .resizable()
+                                            .cornerRadius(15)
+                                            .frame(width: 35, height: 35, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/))
+            }.onAppear(perform: {
+                decodeJSON()
+            })
         }
-        return imageString
     }
+   
+    
+    
     
     // MARK: Parse JSON Data into Model Object
     func decodeJSON() {
@@ -103,6 +71,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewDevice("iPhone SE (2nd generation)")
+            .previewDevice("iPhone 11 Pro")
     }
 }
--- MyPeloton App/WorkoutRowView.swift
+++ MyPeloton App/WorkoutRowView.swift
@@ -4,35 +4,85 @@
 ////
 ////  Created by Alphonso Sensley II on 7/25/20.
 ////
-//
-//import Foundation
-//import SwiftUI
-//
-//
-//struct WorkoutRowView: View {
-//    
-//    @Binding var workouts: Workouts
-//    var workoutNumberSelection = 347
-//    var body: some View {
-//        
-//        List(workouts, id: \.workoutTimestamp) { workout in
-//        HStack {
-//            Image("\(workoutImage(instructorName: workout.instructorName))").resizable().frame(width: 60, height: 70).cornerRadius(25)
-//            
-//            VStack(alignment:.leading) {
-//                Text("\(workout.instructorName)").font(.title)
-//                Text("\(workout.fitnessDiscipline.rawValue)").foregroundColor(.gray)
-//                Text("\(workout.title)").foregroundColor(.gray)
-//                Text("\(workout.caloriesBurned) calories burned").foregroundColor(.gray)
-//                //Text("\(AvgCadenceRPM(from: workout.totalOutput as! Decoder)) ")
-//                Text("\(workout.workoutTimestamp)")
-//                    .foregroundColor(.blue)
-//                
-//            }
-//        }
-//     }
-//  }
-//    
+
+import Foundation
+import SwiftUI
+
+
+struct WorkoutRowView: View {
+    
+    var workouts: Workouts
+    var body: some View {
+        
+        List(workouts, id: \.workoutTimestamp) { workout in
+            ZStack(alignment: .leading) {
+                HStack {
+                    Image("\(getWorkoutImage(instructorName: workout.instructorName))")
+                        .resizable()
+                        .frame(width: 59, height: 70)
+                        .cornerRadius(40)
+                    VStack(alignment:.leading) {
+                        Text("\(getWorkoutInstructorName(name:workout.instructorName))")
+                            .font(.title)
+                        Text("\(workout.fitnessDiscipline.rawValue)")
+                            .foregroundColor(.gray)
+                        Text("\(getWorkoutTitle(title: workout.title))")
+                            .foregroundColor(.gray)
+                            .font(.subheadline)
+                        Text("\(getWorkoutTimeStamp(timeStamp: workout.workoutTimestamp))")
+                            .foregroundColor(.blue)
+                            .font(.subheadline)
+                    }
+                }
+                
+                NavigationLink(
+                    destination: WorkoutChartView(),
+                    label: {})
+            }
+        }
+    }
+    //MARK: String Validation Methods
+    
+    func getWorkoutImage(instructorName:String) ->String {
+        var imageString = String()
+        if instructorName.isEmpty {
+            imageString = "Peloton"
+        } else {
+            imageString = instructorName
+        }
+        return imageString
+    }
+    
+    func getWorkoutInstructorName(name:String) ->String {
+        var nameString = String()
+        if name.isEmpty {
+            nameString = "Scenic Ride"
+        } else {
+            nameString = name
+        }
+        return nameString
+    }
+    
+    func getWorkoutTitle(title:String) ->String {
+        var titleString = String()
+        if title.isEmpty {
+            titleString = "Unknown Workout"
+        } else {
+            titleString = title
+        }
+        return titleString
+    }
+    
+    func getWorkoutTimeStamp(timeStamp:String) ->String {
+        var timeStampString = String()
+        if timeStamp.isEmpty {
+            timeStampString = "Unknown Time"
+        } else {
+            timeStampString = timeStamp
+        }
+        return timeStampString
+    }
+}
 //    
 //}
 //
